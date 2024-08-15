3678dde0e4d9e048201a80ee4c0b0b23f3b3b0f3
Androff <rageclock23@gmail.com>
2024-08-15 17:51:57 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let skoby = bracketsConfig.reduce((acc, [key, val]) => {
+    return (acc[key] = val), acc;
+  }, {});
+
+  for (let i = 0; i < str.length; i++) {
+    if (Object.keys(skoby).includes(str[i])) {
+      if (
+        Object.values(skoby).includes(str[i]) &&
+        skoby[stack[stack.length - 1]] == str[i]
+      ) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else {
+      if (stack.length == 0 || skoby[stack.pop()] !== str[i]) {
+        return false;
+      }
+    }
+  }
+  return stack.length == 0;
+};
